f390d62e163161c2dd6a72fab9a4b13e76ff7d0e
GeorgeGabor96 <gaborgeorge96@gmail.com>
2023-06-18 12:26:20 +0300

    Chapter 11

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 mkdir build
 pushd lox
 
-javac Lox.java Environment.java Expr.java Interpreter.java Parser.java RuntimeError.java Scanner.java Stmt.java Token.java TokenType.java LoxCallable.java LoxFunction.java Return.java -d ..\build
+javac Lox.java Environment.java Expr.java Interpreter.java Parser.java RuntimeError.java Scanner.java Stmt.java Token.java TokenType.java LoxCallable.java LoxFunction.java Return.java Resolver.java -d ..\build
 
 popd
 
